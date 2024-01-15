615c5a5c758140a1fd64570a7bdd63f1c5893bb0
Camilo Bello <camilo_bello@epam.com>
2024-01-15 11:55:08 -0500

    Refactor

--- js/events/0.4.js
+++ js/events/0.4.js
@@ -1,40 +1,54 @@
-let currentPage = 1;
+let currentPage = 0;
 $(() => {
     $('#more-photos').click((e) => {
         e.preventDefault();
         currentPage++;
-
         const url = $(e.target).attr('href').replace('$param', currentPage);
+        getImageData(url);
+    });
 
+    const url = $('#more-photos').attr('href').replace('$param', currentPage);
+    getImageData(url);
+
+    function renderImage(element) {
+        $('#gallery').append(`<div class="photo">
+            <img src="${element.download_url}" alt="Imagen ${element.id}">
+            <div class="details">
+                <p>${element.author}</p>
+            </div>
+        </div>`);
+    }
+
+    function getImageData(url) {
         $.get(url)
             .then(data => {
                 data.forEach(element => {
-                    $('#gallery').append(`<div class="photo">
-                    <img src="${element.download_url}" alt="Imagen ${element.id}">
-                    <div class="details">
-                        <p>${element.author}</p>
-                    </div>
-                </div>`) 
+                    renderImage(element);
                 });
 
-                $('div.photo').off('mouseenter mouseleave')
+                $('div.photo').off('mouseenter mouseleave');
                 initEvents();
             })
-            .catch(({statusText}) => {
-                $('#gallery').append(`<strong>${statusText}</strong>`) 
-            })
-    });
-    initEvents();
-
+            .catch(({ statusText }) => {
+                $('#gallery').append(`<strong>${statusText}</strong>`);
+            });
+    }
     function initEvents() {
         $('div.photo').on('mouseenter mouseleave', (e) => {
-            console.log('handle' , e.type)
-            const $details = $(e.currentTarget).find('.details');
+            const $target = $(e.currentTarget);
+            const $details = $target.find('.details');
             if (e.type === 'mouseenter') {
                 $details.fadeTo('fast', 0.7);
+                $target.animate({
+                    padding: 0
+                })
             } else {
                 $details.fadeOut('fast');
+                $target.animate({
+                    padding: 15
+                })
             }
         });
     }
+
 });
