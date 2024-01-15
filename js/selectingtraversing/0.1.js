f57a610e153e8b75816f285a6c37787e3619adc8
Camilo Bello <camilo_bello@epam.com>
2024-01-15 15:11:00 -0500

    Sorting table

--- js/selectingtraversing/0.1.js
+++ js/selectingtraversing/0.1.js
@@ -32,4 +32,37 @@ $(() => {
         stripe();
      });
 
+     $('table')
+     .on('click', 'th', function () {
+        let index = $(this).index(),
+          rows = [],
+          thClass = $(this).hasClass('asc') ? 'desc' : 'asc';
+
+        $('th').removeClass('asc desc');
+        $(this).addClass(thClass);
+
+        $('tbody tr').each(function (index, row) {
+            rows.push($(row).detach());
+        });
+
+        rows.sort(function (a, b) {
+            var aValue = $(a).find('td').eq(index).text(),
+                bValue = $(b).find('td').eq(index).text();
+
+            return aValue > bValue
+                ? 1
+                : aValue < bValue
+                ? -1
+                : 0;
+        });
+
+        if ($(this).hasClass('desc')) {
+            rows.reverse();
+        }
+
+        $.each(rows, function (index, row) {
+            $('tbody').append(row);
+        });
+     });
+
 });
